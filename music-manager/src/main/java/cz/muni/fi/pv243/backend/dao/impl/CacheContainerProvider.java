7950430463a4fea640d55f2d5ef7ac3beae69e98
Radek.Koubsky <radekkoubsky@gmail.com>
2014-05-08 13:23:20 +0200

    Radek and Daniel - configuration of BasicCacheContainer using LevelDB cache store, each entity has its own cache.
    Signed-off-by: Radek.Koubsky <radekkoubsky@gmail.com>

---
+++ music-manager/src/main/java/cz/muni/fi/pv243/backend/dao/impl/CacheContainerProvider.java
@@ -0,0 +1,66 @@
+package cz.muni.fi.pv243.backend.dao.impl;
+
+
+
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.ApplicationScoped;
+
+import org.infinispan.commons.api.BasicCacheContainer;
+import org.infinispan.configuration.cache.CacheMode;
+import org.infinispan.configuration.cache.Configuration;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.global.GlobalConfiguration;
+import org.infinispan.configuration.global.GlobalConfigurationBuilder;
+import org.infinispan.eviction.EvictionStrategy;
+import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.persistence.leveldb.configuration.LevelDBStoreConfiguration;
+import org.infinispan.persistence.leveldb.configuration.LevelDBStoreConfigurationBuilder;
+import org.infinispan.transaction.LockingMode;
+import org.infinispan.transaction.TransactionMode;
+import org.infinispan.transaction.lookup.GenericTransactionManagerLookup;
+import org.infinispan.util.concurrent.IsolationLevel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@ApplicationScoped
+public class CacheContainerProvider {
+	static final Logger logger = LoggerFactory.getLogger(CacheContainerProvider.class);
+
+    private BasicCacheContainer manager;
+
+    public BasicCacheContainer getCacheContainer() {
+        if (manager == null) {
+        	logger.info("Initialization of BasicCacheContainer...");
+        	GlobalConfiguration glob = new GlobalConfigurationBuilder()
+        	                          .nonClusteredDefault().globalJmxStatistics().enable()
+        	                          .jmxDomain("cz.muni.fi.pv243.musicmanager")  //prevent collision with non-transactional carmart
+        	                          .build();
+        	
+        	Configuration defaultConfig = new ConfigurationBuilder()
+        	               .transaction().transactionMode(TransactionMode.TRANSACTIONAL)
+        	               .build();  //default config
+        	
+        	Configuration musicCacheConfig = new ConfigurationBuilder().persistence()
+        									.addStore(LevelDBStoreConfigurationBuilder.class)
+        									.location("C:\\leveldb\\data")
+        									.expiredLocation("C:\\leveldb\\expired")
+        									.build();
+        	
+        	manager = new DefaultCacheManager(glob, defaultConfig);
+        	//defining cache for each entity
+        	((DefaultCacheManager) manager).defineConfiguration("usercache", musicCacheConfig);
+        	((DefaultCacheManager) manager).defineConfiguration("songcache", musicCacheConfig);
+        	((DefaultCacheManager) manager).defineConfiguration("commentcache", musicCacheConfig);
+        	((DefaultCacheManager) manager).defineConfiguration("interpretcache", musicCacheConfig);
+        	manager.start();
+        	logger.info("Using DefaultCacheManager (library mode)");
+        }
+        return manager;
+    }
+
+    @PreDestroy
+    public void cleanUp() {
+        manager.stop();
+        manager = null;
+    }
+}
