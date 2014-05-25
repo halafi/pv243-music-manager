87b2123417c6fe2fab12a8f5380fe044f53ea5fc
Dandzi <dsdandzi@gmail.com>
2014-05-25 09:44:34 +0200

    User, UserManager, UserManagerImpl
    User updated, UserManager updated + added javadoc, UserManagerImpl
    implementated

--- music-manager/src/main/java/cz/muni/fi/pv243/musicmanager/dao/UserManager.java
+++ music-manager/src/main/java/cz/muni/fi/pv243/musicmanager/dao/UserManager.java
@@ -1,10 +1,67 @@
 package cz.muni.fi.pv243.musicmanager.dao;
 
-import javax.ejb.Local;
+import java.util.List;
+
 
 import cz.muni.fi.pv243.musicmanager.entities.User;
-@Local
-public interface UserManager {
-	public User createUser(User user);
+import cz.muni.fi.pv243.musicmanager.exceptions.IllegalEntityException;
+import cz.muni.fi.pv243.musicmanager.exceptions.NonExistingEntityException;
 
+
+/**
+ * @author Daniel
+ *
+ */
+public interface UserManager {
+
+
+	/**
+	 * Creates new {@link User}.
+	 * @param User which will be created, users unique value is username.
+	 * @throws IllegalArgumentException if user is null.
+	 * @thorws IllegalEntityException if user already exists
+	 **/
+	public void createUser(User user) throws IllegalArgumentException, IllegalEntityException;
+	
+	
+	/**
+	 * Removes given {@link User}.
+	 * @param user which will be deleted
+	 * @throws IllegalArgumentException if user is null.
+	 * @throws NonExistingEntityException if user does not exist. 
+	 **/
+	public void removeUser(User user) throws IllegalArgumentException, NonExistingEntityException;
+	
+	
+	/**
+	 * Updates given {@link User}.
+	 * @param user which will be updated
+	 * @throws IllegalArgumentException if user is null.
+	 * @throws NonExistingEntityException if user does not exist. 
+	 **/
+	public void updateUser(User user) throws IllegalArgumentException, NonExistingEntityException;
+	
+
+	
+	/**
+	 * @param username, unique value of each user.
+	 * @throws IllegalArgumentException if user is null.
+	 * @return User by username.
+	 **/
+	public User getUserByUsername(String username) throws IllegalArgumentException;
+	
+	
+	/**
+	 * 
+	 * @return List of all existing users.
+	 */
+	public  List<User> getAllUsers();
+	
+	
+	/**
+	 * Removes all {@link User} from cache
+	 **/
+	public void removeAllUsers();
 }
+
+
--- music-manager/src/main/java/cz/muni/fi/pv243/musicmanager/dao/impl/UserManagerImpl.java
+++ music-manager/src/main/java/cz/muni/fi/pv243/musicmanager/dao/impl/UserManagerImpl.java
@@ -1,25 +1,31 @@
 package cz.muni.fi.pv243.musicmanager.dao.impl;
 
-import javax.annotation.Resource;
-import javax.ejb.Stateful;
+import java.util.ArrayList;
+import java.util.List;
+
+
 import javax.ejb.Stateless;
-import javax.enterprise.inject.Model;
 import javax.inject.Inject;
 import javax.transaction.UserTransaction;
 
 import org.infinispan.commons.CacheException;
 import org.infinispan.commons.api.BasicCache;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import cz.muni.fi.pv243.musicmanager.dao.UserManager;
 import cz.muni.fi.pv243.musicmanager.entities.User;
+import cz.muni.fi.pv243.musicmanager.exceptions.IllegalEntityException;
+import cz.muni.fi.pv243.musicmanager.exceptions.NonExistingEntityException;
 
-@Model
-//@Stateless
+/**
+ * @author Daniel
+ *
+ */
+@Stateless
+@javax.ejb.TransactionManagement(javax.ejb.TransactionManagementType.BEAN)
 public class UserManagerImpl implements UserManager{
-	public static final String USER_CACHE_NAME = "usercache";
-	private static final Logger logger = LoggerFactory.getLogger(UserManagerImpl.class);
+	@Inject
+	private  Logger logger;
 	
 	@Inject
 	private CacheContainerProvider provider;
@@ -27,17 +33,25 @@ public class UserManagerImpl implements UserManager{
 	@Inject
     private UserTransaction userTransaction;
     
-	private BasicCache<String, User> userCache;
+	private BasicCache<String, Object> userCache;
 	
 
 	@Override
-	public User createUser(User user) {
-		userCache = provider.getCacheContainer().getCache(USER_CACHE_NAME);
+	public void createUser(User user) throws IllegalEntityException, IllegalEntityException {
+		if(user==null){
+			throw new IllegalArgumentException("User is null!");
+		}
+		
+		userCache = provider.getCacheContainer().getCache("usercache");
+		
+		if(userCache.containsKey(user.getUsername())){
+			throw new IllegalEntityException("User already exists!");
+		}
 		try {
 			userTransaction.begin();
 			userCache.put(user.getUsername(), user);
 			userTransaction.commit();
-			logger.info("User \""+user.getUsername()+"\" was put to the cache.");
+			logger.info("User "+user.getUsername()+" was put to the cache.");
 		} catch (Exception trEx) {
 			if(userTransaction != null){
 				try {
@@ -46,9 +60,118 @@ public class UserManagerImpl implements UserManager{
 					logger.error("Transaction rollback error.", rbEx);
 				}
 			}
-			logger.error("Error while trying to put user \""+user.getUsername()+"\" to the cache.", trEx);
+			logger.error("Error while trying to put user "+user.getUsername()+"to the cache.", trEx);
 			throw new CacheException(trEx); 
 		}
-		return null;
+
+	}
+
+
+	@Override
+	public void removeUser(User user) throws IllegalArgumentException, NonExistingEntityException{
+		if(user==null){
+			throw new IllegalArgumentException("User is null!");
+		}
+		
+		userCache = provider.getCacheContainer().getCache("usercache");
+		
+		if(!userCache.containsKey(user.getUsername())){
+			throw new NonExistingEntityException("User does not exists!");
+		}
+		try {
+			userTransaction.begin();
+			userCache.remove(user.getUsername(), user);
+			userTransaction.commit();
+			logger.info("User "+user.getUsername()+" was removed from the cache.");
+		} catch (Exception trEx) {
+			if(userTransaction != null){
+				try {
+					userTransaction.rollback();
+				} catch (Exception rbEx) {
+					logger.error("Transaction rollback error.", rbEx);
+				}
+			}
+			logger.error("Error while trying to remove user "+user.getUsername()+"to the cache.", trEx);
+			throw new CacheException(trEx); 
+		}
+	}
+
+
+	@Override
+	public void updateUser(User user) throws IllegalArgumentException, NonExistingEntityException {
+		if(user==null){
+			throw new IllegalArgumentException("User is null!");
+		}
+		
+		userCache = provider.getCacheContainer().getCache("usercache");
+		
+		if(!userCache.containsKey(user.getUsername())){
+			throw new NonExistingEntityException("User does not exists!");
+		}
+		try{
+			userTransaction.begin();
+			userCache.put(user.getUsername(), user);
+			userTransaction.commit();
+			logger.info("User "+user.getUsername()+" was updated back to the cache.");
+		} catch (Exception trEx) {
+			if(userTransaction != null){
+				try {
+					userTransaction.rollback();
+				} catch (Exception rbEx) {
+					logger.error("Transaction rollback error.", rbEx);
+				}
+			}
+			logger.error("Error while trying to update user "+user.getUsername()+"to the cache.", trEx);
+			throw new CacheException(trEx); 
+		}
+		
+	}
+
+
+	@Override
+	public User getUserByUsername(String username) throws IllegalArgumentException{
+		if(username==null){
+			throw new IllegalArgumentException("Param. username is null");
+		}
+		userCache = provider.getCacheContainer().getCache("usercache");
+		
+		if(userCache.containsKey(username))
+			return (User) userCache.get(username);
+		else
+			return null;
+	}
+
+
+	@Override
+	public List<User> getAllUsers() {
+		userCache = provider.getCacheContainer().getCache("usercache");
+		List<User> users = new ArrayList<User>();
+		for(String username : userCache.keySet()){
+			users.add((User) userCache.get(username));
+		}
+		return users;
+	}
+
+
+	@Override
+	public void removeAllUsers() {
+		userCache = provider.getCacheContainer().getCache("usercache");
+		try{
+			userTransaction.begin();
+			userCache.clear();
+			userTransaction.commit();
+		}catch  (Exception trEx) {
+			if(userTransaction != null){
+				try {
+					userTransaction.rollback();
+				} catch (Exception rbEx) {
+					logger.error("Transaction rollback error.", rbEx);
+				}
+			}
+			logger.error("Error while trying to remove all users from cache", trEx);
+			throw new CacheException(trEx); 
+		}
+		
 	}
 }
+
