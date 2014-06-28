a39cf5f17a1e5809f8c74d0b9302012e5fddbe96
Terrorhunt <roman.macor@gmial.com>
2014-06-28 16:54:47 +0200

    Complete CRUD on JSF - interpret

--- music-manager/src/main/java/cz/muni/fi/pv243/musicmanager/controller/InterpretController.java
+++ music-manager/src/main/java/cz/muni/fi/pv243/musicmanager/controller/InterpretController.java
@@ -20,6 +20,7 @@ import cz.muni.fi.pv243.musicmanager.services.InterpretService;
 
 /**
  * Interpret JSF controller for the {@link Interpret} entity.
+ * 
  * @author Roman Macor
  */
 
@@ -28,57 +29,123 @@ public class InterpretController {
 
 	@Inject
 	private FacesContext facesContext;
-	
+
 	@Inject
 	private ResourceBundle bundle;
-	
+
 	@Inject
-	private  InterpretService interpretService;
-	
+	private InterpretService interpretService;
+
 	@Inject
 	private Logger logger;
-	
+
 	@Produces
 	@Named
 	private Interpret interpret;
 	
 	@PostConstruct
-	public void initInterpret(){
-		interpret= new Interpret();
+	public void initInterpret() {
+		interpret = new Interpret();
 	}
-	
+
 	public List<Interpret> getAllInterprets() {
 		try {
-			//return songService.getSongsbyInterpret("Unknown");
+			// return songService.getSongsbyInterpret("Unknown");
 			return interpretService.getAllInterprets();
 		} catch (ServiceException e) {
-			facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
-					bundle.getString("interpret.getAll.fail.service"), null));
+			facesContext.addMessage(
+					null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, bundle
+							.getString("interpret.getAll.fail.service"), null));
 		}
 		return null;
 	}
-	
-	public void addInterpret() {
+
+	public String addInterpret() {
 		try {
 			interpretService.createInterpret(interpret);
-			
-			facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, 
-					bundle.getString("interpret.add.success"), null));
+
+			facesContext.addMessage(
+					null,
+					new FacesMessage(FacesMessage.SEVERITY_INFO, bundle
+							.getString("interpret.add.success"), null));
 			logger.info(interpret.getName() + " created ");
 			initInterpret();
-		
+			return "index";
 		} catch (ServiceException e) {
-			facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
-					bundle.getString("interpret.add.fail.service"), null));
-		} catch (Exception e) {
-			facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
-					bundle.getString("interpet.add.fail.unknown") + e.getMessage(), null));
+			facesContext.addMessage(
+					null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, bundle
+							.getString("interpret.add.fail.service"), null));
+			return "";
+		}
+	}
+
+	public String updateInterpret() {
+		try {
+			interpretService.updateInterpret(interpret);
+			
+			facesContext.addMessage(
+					null,
+					new FacesMessage(FacesMessage.SEVERITY_INFO, bundle
+							.getString("interpret.update.success"), null));
+			logger.info(interpret.getName() + " created ");
+			initInterpret();
+			return "index";
+		} catch (ServiceException e) {
+			facesContext.addMessage(
+					null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, bundle
+							.getString("interpret.update.fail.service"), null));
+			return "";
 		}
 	}
 	
-	public Genre[] getGenres(){
-		return Genre.values();
-	
+	public void removeInterpret(String id) {
+		try {
+			Interpret interpret = interpretService.getInterpretById(id);
+			interpretService.deleteInterpret(interpret);
+
+			facesContext.addMessage(
+					null,
+					new FacesMessage(FacesMessage.SEVERITY_INFO, bundle
+							.getString("interpret.remove.success"), null));
+		} catch (ServiceException e) {
+			facesContext.addMessage(
+					null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, bundle
+							.getString("interpret.remove.fail.service"), null));
+		}
 	}
-	
+
+	public void removeAllInterprets() {
+		try {
+			interpretService.removeAllInterprets();
+			facesContext.addMessage(
+					null,
+					new FacesMessage(FacesMessage.SEVERITY_INFO, bundle
+							.getString("interpret.remove.success"), null));
+		} catch (ServiceException e) {
+			facesContext.addMessage(
+					null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, bundle
+							.getString("interpret.remove.fail.service"), null));
+		}
+	}
+
+	public String editInterpret(String id) {
+		try {
+			interpret = interpretService.getInterpretById(id);
+		} catch (ServiceException e) {
+			facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+					bundle.getString("interpret.get.fail.service"), null));
+		}
+		return "edit";
+	}
+
+	public Genre[] getGenres() {
+		return Genre.values();
+
+	}
+
 }
